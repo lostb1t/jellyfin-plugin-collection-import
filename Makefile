94b2a8f846f1229cb163ae940b2ca6bd5b98a51d
lostb1t <coding-mosses0z@icloud.com>
2024-08-18 14:31:27 +0200

    add release flow

--- Makefile
+++ Makefile
@@ -1,5 +1,29 @@
+export VERSION := 0.9
+export GITHUB_REPO := lostb1t/jellyfin-plugin-collection-import
+export FILE := collection-import-${VERSION}.zip
+
+build: 
+	dotnet build
+
 zip:
-	zip collectionimport.zip Jellyfin.Plugin.CollectionImport/bin/Debug/net8.0/Jellyfin.Plugin.CollectionImport.dll 
+	zip ${FILE} Jellyfin.Plugin.CollectionImport/bin/Debug/net8.0/Jellyfin.Plugin.CollectionImport.dll 
 
 csum:
-	md5sum collectionimport.zip
+	md5sum ${FILE} 
+
+create-tag:
+	git tag ${VERSION}
+	git push origin ${VERSION}
+
+create-gh-release:
+	gh release create ${VERSION} ${FILE} --generate-notes --verify-tag
+
+update-files:
+	node scripts/update-version.js
+	node scripts/validate-and-update-manifest.js
+
+push-manifest:
+	git commit -m 'new release' manifest.json
+	git push origin main
+
+release: build zip create-tag create-gh-release update-files push-manifest
---
+++ scripts/validate-and-update-manifest.js
@@ -0,0 +1,132 @@
+const https = require('https');
+const crypto = require('crypto');
+const fs = require('fs');
+const { URL } = require('url');
+
+const repository = process.env.GITHUB_REPO;
+const version = process.env.VERSION;
+const file = process.env.FILE;
+const targetAbi = "10.9.9.0";
+
+console.log(file);
+// Read manifest.json
+const manifestPath = './manifest.json';
+if (!fs.existsSync(manifestPath)) {
+    console.error('manifest.json file not found');
+    process.exit(1);
+}
+
+// Read README.md
+// const readmePath = './README.md';
+// if (!fs.existsSync(readmePath)) {
+//     console.error('README.md file not found');
+//     process.exit(1);
+// }
+
+const jsonData = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
+
+const newVersion = {
+    version,
+    changelog: `- See the full changelog at [GitHub](https://github.com/${repository}/releases/tag/${version})\n`,
+    targetAbi,
+    sourceUrl: `https://github.com/${repository}/releases/download/${version}/${file}`, 
+    checksum: getMD5FromFile(),
+    timestamp: new Date().toISOString().replace(/\.\d{3}Z$/, 'Z')
+};
+
+async function updateManifest() {
+    await validVersion(newVersion);
+
+    // Add the new version to the manifest
+    jsonData[0].versions.unshift(newVersion);
+
+    // Write the updated manifest to file if validation is successful
+    fs.writeFileSync(manifestPath, JSON.stringify(jsonData, null, 4));
+    console.log('Manifest updated successfully.');
+    //updateReadMeVersion();
+    process.exit(0); // Exit with no error
+}
+
+async function validVersion(version) {
+    console.log(`Validating version ${version.version}...`);
+
+    const isValidChecksum = await verifyChecksum(version.sourceUrl, version.checksum);
+    if (!isValidChecksum) {
+        console.error(`Checksum mismatch for URL: ${version.sourceUrl}`);
+        process.exit(1); // Exit with an error code
+    } else {
+        console.log(`Version ${version.version} is valid.`);
+    }
+}
+
+async function verifyChecksum(url, expectedChecksum) {
+    try {
+        const hash = await downloadAndHashFile(url);
+        return hash === expectedChecksum;
+    } catch (error) {
+        console.error(`Error verifying checksum for URL: ${url}`, error);
+        return false;
+    }
+}
+
+async function downloadAndHashFile(url, redirects = 5) {
+    if (redirects === 0) {
+        throw new Error('Too many redirects');
+    }
+
+    return new Promise((resolve, reject) => {
+        https.get(url, (response) => {
+            if (response.statusCode >= 300 && response.statusCode < 400 && response.headers.location) {
+                // Follow redirect
+                const redirectUrl = new URL(response.headers.location, url).toString();
+                downloadAndHashFile(redirectUrl, redirects - 1)
+                    .then(resolve)
+                    .catch(reject);
+            } else if (response.statusCode === 200) {
+                const hash = crypto.createHash('md5');
+                response.pipe(hash);
+                response.on('end', () => {
+                    resolve(hash.digest('hex'));
+                });
+                response.on('error', (err) => {
+                    reject(err);
+                });
+            } else {
+                reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
+            }
+        }).on('error', (err) => {
+            reject(err);
+        });
+    });
+}
+
+function getMD5FromFile() {
+    const fileBuffer = fs.readFileSync(`${file}`);
+    return crypto.createHash('md5').update(fileBuffer).digest('hex');
+}
+
+// function getReadMeVersion() {
+//     let parts = targetAbi.split('.').map(Number);
+//     parts.pop();
+//     return parts.join(".");
+// }
+
+// function updateReadMeVersion() {
+//     const newVersion = getReadMeVersion();
+//     const readMeContent = fs.readFileSync(readmePath, 'utf8');
+
+//     const updatedContent = readMeContent
+//         .replace(/Jellyfin.*\(or newer\)/, `Jellyfin ${newVersion} (or newer)`)
+//     if (readMeContent != updatedContent) {
+//         fs.writeFileSync(readmePath, updatedContent);
+//         console.log('Updated README with new Jellyfin version.');
+//     } else {
+//         console.log('README has already newest Jellyfin version.');
+//     }
+// }
+
+async function run() {
+    await updateManifest();
+}
+
+run();
